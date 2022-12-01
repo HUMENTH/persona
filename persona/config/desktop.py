9ad16da281917d9955380869e8b7b7a6f00bd89c
Himanshu Shivhare <himanshushivhare047@gmail.com>
2022-12-01 18:52:19 +0530

    Icon remove

--- persona/config/desktop.py
+++ persona/config/desktop.py
@@ -4,8 +4,8 @@ def get_data():
 	return [
 		{
 			"module_name": "Persona",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
+			#"color": "grey",
+			#"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Persona")
 		}
